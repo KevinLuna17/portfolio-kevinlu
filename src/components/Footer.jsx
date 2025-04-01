dc00edcf583595f7a843ec53cc431eea802d6035
Kevinlu <kelugo_14@hotmail.com>
2025-04-01 14:23:05 -0400

    Improve styles and refactor the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,44 @@
+import Github from "./icons/Github";
+import LinkedIn from "./icons/Linkedin";
+import YouTube from "./icons/Youtube";
+
+const currentYear = new Date().getFullYear();
 function Footer() {
   return (
     <section className="py-20 bg-gray-50">
       <div className="container md:max-w-2xl lg:max-w-4xl mx-auto px-4">
         <div className="flex flex-col items-center justify-center gap-10">
           <ul className="inline-flex items-center justify-center gap-8">
-            <li>LinkedIn</li>
-            <li>Github</li>
-            <li>Youtube</li>
+            <li>
+              <a
+                href="https://github.com/KevinLuna17"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/kevin-luna-gonzalez-a2168b194/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedIn />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/@MrKevin199714"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <YouTube />
+              </a>
+            </li>
           </ul>
           <span className="text-sm text-center text-gray-600">
-            © 2025 Kevin Luna. Todos los derechos reservados.
+            © {currentYear} Kevin Luna. Todos los derechos reservados.
           </span>
         </div>
       </div>
